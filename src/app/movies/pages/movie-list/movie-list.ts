dd3787eef5c4abd3760a2805ab9b69fe8e1e0631
alexis.rueda <alexisrueda34@gmail.com>
2026-09-23 14:05:30 -0600

    feat: implement movie list with pagination and enhance layout components

--- src/app/movies/pages/movie-list/movie-list.ts
+++ src/app/movies/pages/movie-list/movie-list.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MoviesService } from '@movies/services/movies.service';
+
+import { rxResource } from '@angular/core/rxjs-interop';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Pagination } from '@shared/components/pagination/pagination';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [],
+  imports: [RouterLink, DecimalPipe, DatePipe, Pagination],
   templateUrl: './movie-list.html',
 })
-export class MovieList {}
+export class MovieList {
+
+  moviesService = inject(MoviesService);
+  paginationService = inject(PaginationService);
+
+  moviesResource = rxResource({
+    params: () => ({
+      page: this.paginationService.currentPage()
+     }),
+     stream: ({ params }) => this.moviesService.getMovies(params),
+  });
+
+}
